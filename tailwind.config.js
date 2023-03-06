b5136d820115afda82a6b9f2e6f7bcec4dad2a90
saddamarbaa <saddamarbaas@gamil.com>
2023-03-06 22:04:15 +0700

    deps: add tailwindcss/line-clamp dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,21 @@ module.exports = {
 		'./src/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			lineClamp: {
+				7: '7',
+				8: '8',
+				9: '9',
+				10: '10',
+				11: '11',
+				12: '12',
+			},
+		},
 	},
-	plugins: [],
+	variants: {
+		lineClamp: ['responsive'],
+	},
+	plugins: [require('@tailwindcss/line-clamp')],
+	mode: 'jit',
 	darkMode: 'class',
 }
