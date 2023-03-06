16392deb106aabd9a76e45c5b5f008240d3aa0bd
saddamarbaa <saddamarbaas@gamil.com>
2023-03-06 22:05:38 +0700

    fix: fix next image domain configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		domains: ['image.tmdb.org'],
+	},
 }
 
 module.exports = nextConfig
